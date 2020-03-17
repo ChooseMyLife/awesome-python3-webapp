24ae1e30740f7760531a159c7068633a38c3b7eb
1102909182 <1102909182@qq.com>
2020-03-17 15:02:53 +0800

    修正代码

--- WWW/orm.py
+++ WWW/orm.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+
 import aiomysql
 
 
@@ -10,63 +11,61 @@ def log(sql, args=()):
 async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
-    # 初始化数据库连接
     __pool = await aiomysql.create_pool(
         host=kw.get('host', 'localhost'),
         port=kw.get('port', 3306),
         user=kw['user'],
         password=kw['password'],
         db=kw['db'],
-        charset=kw.get('charset', 'utf-8'),
-        autocomit=kw.get('autocomit', True),
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
         maxsize=kw.get('maxsize', 10),
         minsize=kw.get('minsize', 1),
         loop=loop
     )
 
-# 查询操作
-
 
 async def select(sql, args, size=None):
     log(sql, args)
     global __pool
-    with (await __pool) as conn:
-        # 创建游标
-        cur = await conn.cursor(aiomysql.DictCursor)
-        # 替换SQL语句的占位符是?，而MySQL的占位符是%s
-        await cur.execute(sql.replace('?', '%s'), args or())
-        if size:
-            rs = await cur.fetchmany(size)
-        else:
-            rs = await cur.fetchall()
-        await cur.close()
-        logging.info('rows returned:%s' & len(rs))
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?', '%s'), args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
+        logging.info('rows returned: %s' % len(rs))
         return rs
 
-# 修改删除操作
 
-
-async def execute(sql, args):
+async def execute(sql, args, autocommit=True):
     log(sql)
-    with (await __pool) as coon:
+    async with __pool.get() as conn:
+        if not autocommit:
+            await conn.begin()
         try:
-            cur = await coon.curson()
-            await cur.execute(sql.repalce('?', '%s'), args)
-            affected = cur.rowcount
-            await cur.close()
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replace('?', '%s'), args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
         except BaseException as e:
-            raise e
-    return affected
+            if not autocommit:
+                await conn.rollback()
+            raise
+        return affected
 
 
 def create_args_string(num):
     L = []
-    for n in range(num):
+    for _ in range(num):
         L.append('?')
     return ', '.join(L)
 
 
 class Field(object):
+
     def __init__(self, name, column_type, primary_key, default):
         self.name = name
         self.column_type = column_type
@@ -74,63 +73,67 @@ class Field(object):
         self.default = default
 
     def __str__(self):
-        return '<%s,%s:%s>' % (self.__class__.__name__, self.column_type, self.name)
-
-# str映射到数据库的varchar
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
 
 class StringField(Field):
+
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
         super().__init__(name, ddl, primary_key, default)
 
 
-class IntegerField(Field):
-    def __init__(self, name=None, primary_key=False, default=None):
-        super().__init__(name, 'bigint', primary_key, default)
+class BooleanField(Field):
 
-
-class Boolean(Field):
-    def __init__(self, name=None, default=None):
+    def __init__(self, name=None, default=False):
         super().__init__(name, 'boolean', False, default)
 
 
-class FloadFiedl(Field):
+class IntegerField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+
+class FloatField(Field):
+
     def __init__(self, name=None, primary_key=False, default=0.0):
         super().__init__(name, 'real', primary_key, default)
 
 
 class TextField(Field):
-    def __init__(self, name=None, primary_key=False, default=None):
-        super().__init__(name, 'text', primary_key, default)
+
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
 
 
 class ModelMetaclass(type):
+
     def __new__(cls, name, bases, attrs):
         if name == 'Model':
             return type.__new__(cls, name, bases, attrs)
         tableName = attrs.get('__table__', None) or name
-        logging.info('found model :%s (talbes:)%s' % (name, tableName))
+        logging.info('found model: %s (table: %s)' % (name, tableName))
         mappings = dict()
         fields = []
         primaryKey = None
         for k, v in attrs.items():
             if isinstance(v, Field):
-                logging.info('found mapping:%s ==>%s' % (k, v))
+                logging.info('  found mapping: %s ==> %s' % (k, v))
                 mappings[k] = v
                 if v.primary_key:
-                    # 找到主键
+                    # 找到主键:
                     if primaryKey:
                         raise Exception(
-                            'Duplicate primary key for field:%s' % k)
+                            'Duplicate primary key for field: %s' % k)
                     primaryKey = k
                 else:
                     fields.append(k)
         if not primaryKey:
-            raise Exception('Primary Key not found.')
+            raise Exception('Primary key not found.')
         for k in mappings.keys():
             attrs.pop(k)
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
-        attrs['__mapping__'] = mappings
+        attrs['__mappings__'] = mappings  # 保存属性和列的映射关系
         attrs['__table__'] = tableName
         attrs['__primary_key__'] = primaryKey  # 主键属性名
         attrs['__fields__'] = fields  # 除主键外的属性名
@@ -145,7 +148,8 @@ class ModelMetaclass(type):
         return type.__new__(cls, name, bases, attrs)
 
 
-class Model(dict, metacalss=ModelMetaclass):
+class Model(dict, metaclass=ModelMetaclass):
+
     def __init__(self, **kw):
         super(Model, self).__init__(**kw)
 
@@ -164,16 +168,17 @@ class Model(dict, metacalss=ModelMetaclass):
     def getValueOrDefault(self, key):
         value = getattr(self, key, None)
         if value is None:
-            field = self.__mapping__[key]
-            if field.default() if callable(field.default) else field.default:
-                logging.debug('using default value for %s:%s' %
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug('using default value for %s: %s' %
                               (key, str(value)))
                 setattr(self, key, value)
         return value
 
     @classmethod
     async def findAll(cls, where=None, args=None, **kw):
-        'find object by where clause.'
+        ' find objects by where clause. '
         sql = [cls.__select__]
         if where:
             sql.append('where')
@@ -191,11 +196,11 @@ class Model(dict, metacalss=ModelMetaclass):
                 sql.append('?')
                 args.append(limit)
             elif isinstance(limit, tuple) and len(limit) == 2:
-                sql.append('?', '?')
+                sql.append('?, ?')
                 args.extend(limit)
             else:
-                raise ValueError('Invalid limit value :%s' % str(limit))
-        rs = await select(''.join(sql), args)
+                raise ValueError('Invalid limit value: %s' % str(limit))
+        rs = await select(' '.join(sql), args)
         return [cls(**r) for r in rs]
 
     @classmethod

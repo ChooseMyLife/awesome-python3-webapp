f57ff250f01f2f8e3d5c6ebe6a381c901fd366ed
1102909182 <1102909182@qq.com>
2020-04-16 10:51:03 +0800

    更新中间件以及修改模板

--- WWW/config_default.py
+++ WWW/config_default.py
@@ -5,7 +5,7 @@ configs = {
         'port': 3306,
         'user': 'root',
         'password': '1234',
-        'database': 'awesome'
+        'db': 'awesome'
     },
     'session': {
         'secret': 'AwEsOmE'
